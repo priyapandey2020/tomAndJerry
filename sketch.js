96835a81b61c3c1d908909062fffd2c65271cc2a
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2020-09-28 15:50:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,43 @@
-var canvas;
+var canvas,bg;
+var together;
 var tom, tomImg1,tomImg2;
-var jerry, jerryImg;
+var jerry, jerryImg1,jerryImg2;
 
 function preload() {
-    
-    tomImg1=loadAnimation("tom.png");
-    tomImg2= loadAnimation("tomsleep.png");
-
-    jerryImg1= loadAnimation("jerry jerry.png");
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    together= loadAnimation("images/together.png");
+    jerryImg1=loadAnimation("images/jerryOne.png")
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
 
 }
 
 function setup(){
-    canvas = createCanvas(1200,500);
+    canvas = createCanvas(1000,800);
 
-    tom = createSprite(1000, 350);
-    tom.addAnimation("tomRunning", tomImg1);
+    tom = createSprite(900, 600);
+    tom.addAnimation("tomSleeping", tomImg1);
     tom.scale = 0.2;
 
-    jerry = createSprite(150, 400);
-    jerry.addAnimation("jerryTeasing", jerryImg1);
-    jerry.scale = 0.4;
+    jerry = createSprite(200, 600);
+    jerry.addAnimation("jerryStanding", jerryImg1);
+    jerry.scale = 0.15;
 
 }
 
 function draw() {
 
-    background("purple");
+    background(bg);
 
-    if(tom.x + jerry.x < (tom.width - jerry.width)/2)
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
     {
         tom.velocityX=0;
-        tom.addAnimation("tomslept", tomImg2);
-        tom.scale= 1;
-        tom.changeAnimation("tomslept");
+        tom.addAnimation("Together", together);
+        tom.scale=0.5;
+        tom.changeAnimation("Together");
 
-        textSize(25);
-        fill(255);
-        text("Tom and Jerry Collided", 100,260);
+        jerry.remove();
     }  
 
     drawSprites();
@@ -46,6 +46,15 @@ function draw() {
 
 function keyPressed(){
     if(keyCode === 32){
+
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
+        
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
+    }
+    if(keyCode === LEFT_ARROW){
         tom.velocityX = -5; 
     }
 }
